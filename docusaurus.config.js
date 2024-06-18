ceabe77edbbe3d7963d14ebb819ad475811879e6
Timurs Boronenko <b.timurs.b@gmail.com>
2024-06-18 23:35:51 +0300

    Fixes for deployment.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,13 +36,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -89,10 +82,6 @@ const config = {
           {
             title: 'Support',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
